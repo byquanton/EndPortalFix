6ab8fd37e0f583264db872750ac0b9666823333e
byquanton <git@akraft.org>
2021-03-17 21:58:27 +0100

    Innital Commit

---
+++ src/main/java/me/byquanton/endportalfix/Endportalfix.java
@@ -0,0 +1,25 @@
+package me.byquanton.endportalfix;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Endportalfix extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this,this);
+    }
+
+    @EventHandler
+    public void oninteract(PlayerInteractEvent event){
+        if(event.getClickedBlock().getBlockData().getMaterial().equals(Material.END_PORTAL_FRAME)){
+            if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+                event.setCancelled(true);
+            }
+        }
+    }
+}
